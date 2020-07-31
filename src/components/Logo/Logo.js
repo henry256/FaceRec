7265b7bf24a41f74ec2c35c958b0e35f886f91d3
Henry Chen <henrychen256@gmail.com>
2020-07-31 17:15:51 -0400

    make it better

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import face from './faceLogo.png';
+const Logo=()=>{
+    return(
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2  shadow-1" options={{ max : 50}} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pd3"> 
+                <img  style={{paddingTop:'25px'}}src={face} alt="Logo"/>
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+export default Logo;
